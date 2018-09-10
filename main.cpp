675c8bbe7983bcc50cea3a1127f6d9f76c6b44c8
gesik92 <krzysztof.gesicki.programista@gmail.com>
2018-09-10 22:38:18 +0200

    wprowadzono opcje edycji oraz usuwania adresata

--- main.cpp
+++ main.cpp
@@ -33,8 +33,10 @@ int addNewContact(int _contactsNumber, vector <Contact> & _contacts) {
     //dodanie nowego kontaktu do tablicy
     cin.ignore();
     _contacts.push_back(Contact());
-    if (_contactsNumber == 0) _contacts[_contactsNumber].id = _contactsNumber+1;
-    else _contacts[_contactsNumber].id = (_contacts[_contactsNumber-1].id+1);
+    if (_contactsNumber == 0)
+        _contacts[_contactsNumber].id = _contactsNumber+1;
+    else
+        _contacts[_contactsNumber].id = (_contacts[_contactsNumber-1].id+1);
     cout << "Podaj imie: ";
     getline(cin, _contacts[_contactsNumber].name);
     cout << endl << "Podaj nazwisko: ";
@@ -110,26 +112,20 @@ void searchContactBySurname(int _contactsNumber, vector <Contact> _contacts) {
     cin.get();
 }
 
-string explodeStringLine (int position, string _textLine)
-{
+string explodeStringLine (int position, string _textLine) {
     string singleObject = "";
     int countPosition = 1;
     int stringSize = _textLine.size();
-    for (int i = 0; i < stringSize; i++)
-    {
-        if (_textLine[i] == '|')
-        {
-            if (countPosition == position)
-            {
+    for (int i = 0; i < stringSize; i++) {
+        if (_textLine[i] == '|') {
+            if (countPosition == position) {
                 return singleObject;
-            }
-            else
-            {
+            } else {
                 countPosition++;
                 singleObject = "";
             }
-        }
-        else singleObject += _textLine[i];
+        } else
+            singleObject += _textLine[i];
     }
 }
 
@@ -177,12 +173,164 @@ void showMenu () {
     cout << "Twoj wybor: ";
 }
 
+void loadVectorToFile (vector <Contact> & _contacts) {
+    fstream textFile;
+    textFile.open(fileName.c_str(), ios::out);
+    if (textFile.good() == false) {
+        cout << "Nie udalo sie otworzyc pliku z Kontaktami!";
+        Sleep(2000);
+        exit(0);
+    }
+    int vectorSize = _contacts.size();
+    for (int i = 0; i < vectorSize; i++) {
+        textFile << _contacts[i].id << "|";
+        textFile << _contacts[i].name << "|";
+        textFile << _contacts[i].surname << "|";
+        textFile << _contacts[i].phoneNumber << "|";
+        textFile << _contacts[i].email << "|";
+        textFile << _contacts[i].address << "|" << endl;
+    }
+    textFile.close();
+}
+
+int deleteContact (vector <Contact> & _contacts) {
+    int id;
+    system("cls");
+    cout << "Podaj ID kontaktu do usuniecia: ";
+    cin >> id;
+    char choice;
+
+    int vectorSize = _contacts.size();
+
+    while (true) {
+        cout << "Na pewno chcesz usunac kontakt o numerze ID: " << id << "? (t/n) ";
+        cin >> choice;
+        if (choice == 'n') {
+            return vectorSize;
+        }
+        if (choice == 't') {
+            int position = -1;
+            for (int i = 0; i < vectorSize; i++) {
+                if (_contacts[i].id == id) {
+                    position = i;
+                    break;
+                }
+            }
+            if (position == -1) {
+                cout << "Nie ma takiego kontaktu!";
+                Sleep(2000);
+                return vectorSize;
+            }
+            _contacts.erase(_contacts.begin()+position);
+            vectorSize = _contacts.size();
+            loadVectorToFile(_contacts);
+            return vectorSize;
+        } else {
+            cout << "Wybierz poprawna opcje!" << endl;
+            continue;
+        }
+    }
+}
+
+void editContact (vector <Contact> & _contacts) {
+    int id;
+    system("cls");
+    cout << "Podaj ID adresata do edycji: ";
+    cin >> id;
+
+    int vectorSize = _contacts.size();
+    int position = -1;
+    for (int i = 0; i < vectorSize; i++) {
+        if (_contacts[i].id == id) {
+            position = i;
+            break;
+        }
+    }
+    if (position == -1) {
+        cout << "Nie ma takiego kontaktu!";
+        Sleep(2000);
+        return;
+    }
+    cout << "Edytujesz ponizszy kontakt:" << endl;
+    cout << "ID kontaktu: " << _contacts[position].id << endl;
+    cout << "Imie: " << _contacts[position].name << endl;
+    cout << "Nazwisko: " << _contacts[position].surname << endl;
+    cout << "Numer telefonu: " << _contacts[position].phoneNumber << endl;
+    cout << "Adres e-mail: " << _contacts[position].email << endl;
+    cout << "Adres: " << _contacts[position].address << endl;
+    cout << endl;
+    cout << "Co chcesz edytowac?" << endl;
+
+    cout << "1-imie" << endl;
+    cout << "2-nazwisko" << endl;
+    cout << "3-numer telefonu" << endl;
+    cout << "4-email" << endl;
+    cout << "5-adres" << endl;
+    cout << "6-powrot do menu" << endl;
+
+    int choice;
+    cin >> choice;
+    string newValue;
+    switch (choice) {
+    case 1:
+        cout << "Nowe imie: ";
+        cin.ignore();
+        getline(cin,newValue);
+        _contacts[position].name = newValue;
+        loadVectorToFile(_contacts);
+        cout << "Zapisano zmiany!";
+        Sleep(1500);
+        break;
+    case 2:
+        cout << "Nowe nazwisko: ";
+        cin.ignore();
+        getline(cin,newValue);
+        _contacts[position].surname = newValue;
+        loadVectorToFile(_contacts);
+        cout << "Zapisano zmiany!";
+        Sleep(1500);
+        break;
+    case 3:
+        cout << "Nowy numer telefonu: ";
+        cin.ignore();
+        getline(cin,newValue);
+        _contacts[position].phoneNumber = newValue;
+        loadVectorToFile(_contacts);
+        cout << "Zapisano zmiany!";
+        Sleep(1500);
+        break;
+    case 4:
+        cout << "Nowy email: ";
+        cin.ignore();
+        getline(cin,newValue);
+        _contacts[position].email = newValue;
+        loadVectorToFile(_contacts);
+        cout << "Zapisano zmiany!";
+        Sleep(1500);
+        break;
+    case 5:
+        cout << "Nowy adres: ";
+        cin.ignore();
+        getline(cin,newValue);
+        _contacts[position].address = newValue;
+        loadVectorToFile(_contacts);
+        cout << "Zapisano zmiany!";
+        Sleep(1500);
+        break;
+    case 6:
+        return;
+    default:
+        cout << "Nie ma takiej opcji!";
+        Sleep(2000);
+        return;
+    }
+}
+
 int main() {
     char selection;
     vector <Contact> contacts;
     int contactsNumber = loadTextFile(contacts);
 
-    //MENU GLOWNE
     while (true) {
 
         showMenu();
@@ -210,12 +358,12 @@ int main() {
 
         else if (selection == '5') {
             system("cls");
-            //deleteContact();
+            contactsNumber = deleteContact(contacts);
         }
 
         else if (selection == '6') {
             system("cls");
-            //editContact();
+            editContact(contacts);
         }
 
         else if (selection == '9') {
